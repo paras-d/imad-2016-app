36a6df29607e92c8becd39ae253876f46626f730
paras-d <m.parasoff@gmail.com>
2016-09-29 19:33:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,61 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var articleOne={
+    title : 'Article one | Paras Dumka',
+    heading: 'Article One',
+    date: 'Sept. 5 2015',
+    content: `  
+           <p>
+                This is the content of my first article. This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article. 
+            </p>
+             <p>
+                This is the content of my first article. This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article. 
+            </p>
+             <p>
+                This is the content of my first article. This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article. 
+            </p>`
+};
+function createtemplate(data)
+{var title= data.title;
+var heading= data.heading;
+var date= data.date;
+var content=data.content;
+var htmltemplate= ` <!--Trying excatly as done in module by tanmai sir-->
+<html>
+<head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content= "width-device-width , initial-scale-1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head>    
+
+<body>
+    <div class="container">
+        
+        <div>
+            <a href="/"> Home </a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+                 
+        </div>
+    </div>
+</body>    
+    
+    
+</html>
+`;
+return htmltemplate;
+}
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
